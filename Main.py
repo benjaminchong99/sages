56aa7fec112d5c70d64dc2e9ba73c9e52dc45639
benjaminchong99 <pschong261199@hotmail.com>
2023-02-22 02:15:00 +0800

    first commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Sages",
+                   layout="wide")
+# st.sidebar.success()
+
+st.title('test')
+
+st.write("Hello World")
---
+++ pages/Quiz.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+st.title('Quiz')
+
+st.write("Hello Quiz")
+
+switch_page("test")
